1736bd8a0910ae11375962efa9f6e0bfb3d4d3d6
Joao Vitor Borge Duclos. <joaovitorborge@gmail.com>
2026-04-08 17:19:59 -0300

    improving intent_classifier

--- chatbot/intent_classifier.py
+++ chatbot/intent_classifier.py
@@ -1,4 +1,5 @@
-from chatbot.nlp import preprocess
+from chatbot.nlp import preprocess #lembrar colocar apenas nlp para testes
+import json
 
 def predict_intent(user_input, intents):
     user_words = set(preprocess(user_input))
@@ -6,26 +7,39 @@ def predict_intent(user_input, intents):
     best_intent = None
     best_match_percent = 0.0
     fallback_intent = None
-    THRESHOLD = 0.5
+    THRESHOLD = 0.1
 
     for intent in intents:
         if intent["tag"] == "conversa_padrao":
             fallback_intent = intent
             continue
+        pattern_words = set()
 
-        for pattern in intent["patterns"]:
-            pattern_words = set(preprocess(pattern))
+        for pattern in intent['patterns']:
+            pattern_words.update(preprocess(pattern))
+        
+        #achei o problema, eu vou ter que criar um for (ou encontrar uma forma mais eficiente) para que todas as palavras da pattern sejam preprocessadas
 
-            common_words = user_words & pattern_words
-            match_percent = len(common_words) / len(pattern_words)
+        common_words = user_words & pattern_words
+        match_percent = len(common_words) / len(pattern_words)
 
-            if match_percent > best_match_percent:
-                best_match_percent = match_percent
-                best_intent = intent
+        if match_percent > best_match_percent:
+            best_match_percent = match_percent
+            best_intent = intent
 
     if best_match_percent >= THRESHOLD:
         return best_intent
-
+        # para testes no próprio arquivo
+        #return [best_intent, best_match_percent]
     return fallback_intent
+    # para testes no próprio arquivo
+    #return [fallback_intent, best_match_percent]
 
-        
+# para testes no próprio arquivo
+
+#with open("data/intents.json", encoding="utf-8") as file:
+#    data = json.load(file)
+#    intents = data["intents"]
+
+# user_input = 'quem é você?'
+# print(predict_intent(user_input, intents))
--- chatbot/nlp.py
+++ chatbot/nlp.py
@@ -4,7 +4,7 @@ def preprocess(text):
     text_lower = text.lower().strip()
     clean_text = "".join(char for char in text_lower if char not in string.punctuation)
     list_of_words = clean_text.split()
-    ignore = set(["de", "um", "uma", "se", "está", "para", "que", "até", "após", "com", "contra", "desde", "em", "entre", "perante", "por", "sem", "sob", "sobre", "trás"])
+    ignore = set(["me", "de", "um", "uma", "se", "está", "para", "que", "até", "após", "com", "contra", "desde", "em", "entre", "perante", "por", "sem", "sob", "sobre", "trás"])
     final_list = []
     for word in list_of_words:
         if len(word) >= 2 and word not in ignore:
